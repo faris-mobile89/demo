d61155ee48853db3d8929849939d933ce6c6e001
Faris <outsource.fn@tra.gov.ae>
2022-12-09 01:33:44 +0400

    Last changes

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Employee;
+use App\Models\Logs;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -11,13 +13,40 @@ class APIController extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
-    public function checkIn()
+    public function checkIn($id)
     {
-        return view('movie.create');
+
+        Logs::create([
+            'employee_id' => $id,
+            'action' => 1,
+            'time' => now()
+        ]);
+
+        return [
+            "status" => true,
+        ];
     }
 
-    public function checkOut()
+    public function checkOut($id)
     {
-        return view('movie.create');
+        Logs::create([
+            'employee_id' => $id,
+            'action' => 2,
+            'time' => now()
+        ]);
+
+        return [
+            "status" => true,
+        ];
     }
+
+    public function account($id)
+    {
+        $employee = Employee::findOrFail($id);
+
+        return $employee;
+    }
+
+
+
 }
